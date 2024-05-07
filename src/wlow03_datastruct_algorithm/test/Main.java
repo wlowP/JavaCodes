9afa466de31cc55578a15e1be1312377a80794c9
wlow <2064954337@qq.com>
2024-05-07 21:05:38 +0800

    算法: 求斐波那契数列通项的递归版(θ(1.618^n)), 递归加备忘版(O(n)), 以及非递归版O(n). 以空间换时间

--- src/wlow03_datastruct_algorithm/test/Main.java
+++ src/wlow03_datastruct_algorithm/test/Main.java
@@ -1,21 +1,9 @@
 package wlow03_datastruct_algorithm.test;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
+        int[] arr = new int[10];
 
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(1);
-
-        System.out.println(list);
-
-        list.remove(Integer.valueOf(1));
-
-        System.out.println(list);
+        System.out.println(arr[5]);
     }
 }
